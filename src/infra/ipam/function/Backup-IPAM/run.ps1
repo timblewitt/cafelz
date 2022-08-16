4a5a273b35b34cf98eba34434cd8c5355e9f2725
timblewitt <timblewitt@hotmail.com>
2022-08-16 08:55:33 +0100

    Backup-IPAM

--- src/infra/ipam/function/Backup-IPAM/run.ps1
+++ src/infra/ipam/function/Backup-IPAM/run.ps1
@@ -17,19 +17,21 @@ Import-Module Az.Storage
 Connect-AzAccount -Identity
 
 # App Setting Parameters
-$StorageAccountName = $env:ipamStorageAccount
+$storageAccountName = $env:ipamStorageAccount
 Write-Host $StorageAccountName
 
 # Generating Account Key & Creating Context 
-$context = New-AzStorageContext -StorageAccountName $StorageAccountName 
+$key = (Get-AzStorageAccount| where {$_.StorageAccountName -eq $storageAccountName} | Get-AzStorageAccountKey)[0].Value
+$context = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $key
 
 # Gather table names in the storage account
 $tables = (Get-AzStorageTable –Context $context).CloudTable | Select-Object Name -ExpandProperty Name
 
 # AzCopy backup
 foreach ($table in $tables) {
-    Write-Host "Table found: $Table"
-    $source = "https://$StorageAccountName.table.core.windows.net/$table"
+    Write-Host "Table found: $table"
+    $source = "https://$storageAccountName.table.core.windows.net/$table"
     Write-Host "URL generated: $source"
-    C:\home\site\wwwroot\Backup-IPAM\AzCopy.exe  /Source:$source /dest:C:\home\site\wwwroot\Backup-IPAM\$table-backup /sourceKey:$key /PayloadFormat:CSV
+    C:\home\site\wwwroot\Backup-IPAM\AzCopy.exe  /Source:$source /dest:Backup-IPAM\$table-backup /sourceKey:$key /PayloadFormat:CSV --overwrite
+#    C:\home\site\wwwroot\Backup-IPAM\AzCopy.exe  /Source:$source /dest:"C:\home\site\wwwroot\Backup-IPAM\$table-backup" /sourceKey:$key /PayloadFormat:CSV
 }
