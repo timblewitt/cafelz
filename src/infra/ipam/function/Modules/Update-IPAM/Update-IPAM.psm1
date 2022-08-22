c6395d0fda05d24ae2c4bbae2d8e0485901b7b09
timblewitt <timblewitt@hotmail.com>
2022-08-22 13:46:11 +0100

    Remove diags

--- src/infra/ipam/function/Modules/Update-IPAM/Update-IPAM.psm1
+++ src/infra/ipam/function/Modules/Update-IPAM/Update-IPAM.psm1
@@ -9,9 +9,7 @@ Function Update-IPAM {
     # Add records to an Azure storage table (storage account name is an application setting configured during function deployment)
     $storageAccount = $env:ipamStorageAccount
     $saTableName = 'ipam'
-    Write-Host "TIMB1:" $storageAccount
     $saCtx = (Get-AzStorageAccount | where {$_.StorageAccountName -eq $storageAccount}).Context
-    Write-Host "TIMB2:" $saCtx
     $saTable = (Get-AzStorageTable –Name $saTableName –Context $saCtx).CloudTable
 
     $unmanagedAddresses = 0
@@ -20,7 +18,6 @@ Function Update-IPAM {
         Set-AzContext -Subscription $sub.Id
         foreach ($ipamRow in Get-AzTableRow -Table $saTable) {
             $vnet = Get-AzVirtualNetwork | where {$ipamRow.NetworkAddress -in $_.AddressSpace.AddressPrefixes}
-            Write-Host "TIMB3:" $ipamRow.VNetName
             if ($vnet -ne $null) {
                 $ipamRow.Subscription = $sub.Name
                 $ipamRow.VNetName = $vnet.Name
